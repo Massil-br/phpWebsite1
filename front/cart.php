6aedbc2da5da21d2b6c88e02b98cf0ce73a84f2a
Massil <160241880+Massil-br@users.noreply.github.com>
2025-07-02 16:14:36 +0200

    objectives db plan

--- front/cart.php
+++ front/cart.php
@@ -5,9 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="icon" href="./assets/logo.png">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./static/style.css">
 </head>
 <body>
-    
+    <?php include './includes/header.php'; ?>
+    <div id="main">
+        <div class="cart-flex">
+            
+        </div>
+    </div>
+    <?php include './includes/footer.php' ; ?>
+    <script src="./static/backGroundAnimation.js" defer></script>
+    <script src="toggleNavBar.js" defer></script>
 </body>
 </html>
