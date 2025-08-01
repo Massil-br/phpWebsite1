0796cbda595d7bdb97b78f416c86cc16598e4084
Massil <160241880+Massil-br@users.noreply.github.com>
2025-08-01 15:24:31 +0200

    cart style fixed, login autocomplete tags ( to improve later)

--- front/cart.php
+++ front/cart.php
@@ -39,33 +39,33 @@ if(isset($response['error'])){
 
 
         <div class="center">
-            <div class="cart d-flex d-flex justify-content-center align-items-center mt-5 flex-column overflow-y-auto mh-80">
+            <div class="cart d-flex d-flex justify-content-center align-items-center mt-5 flex-column overflow-y-auto mh-80 overflow-x-hidden"  >
                 <?php if (!empty($cartProductsDisplay)):
                     foreach($cartProductsDisplay as $cartProductDisplay):
                     ?>
-                    <div class="cart-item d-flex justify-content-center mt-5 gap-5 text-color align-items-center">
-                        <a class="nav-link " href="./product.php?id=<?=$cartProductDisplay->cartProduct->GetProductId() ?>">
+                    <div class="cart-item d-flex justify-content-between mt-3 gap-5 text-color align-items-center flex-wrap bg-reviews w-reviews rounded-2 p-2 ">
+                        <a class="nav-link mb-0 " href="./product.php?id=<?=$cartProductDisplay->cartProduct->GetProductId() ?>">
                             <img src="<?= $cartProductDisplay->variantImage->GetRelativeUrl() ?>" alt="Icône" width="48" height="48">
                         </a>
-                        <a class="nav-link " href="./product.php?id=<?=$cartProductDisplay->cartProduct->GetProductId() ?>">
+                        <a class="nav-link mx-1 mb-0 " href="./product.php?id=<?=$cartProductDisplay->cartProduct->GetProductId() ?>">
                             <h4 class="cart-p-name"><?= $cartProductDisplay->variant->GetName()?></h4>
                         </a>
                             <?php foreach($cartProductDisplay->cartProduct->variantAttributes as $variantAttribute) :?>
-                            <p class="cart-attr m-0"><?= $variantAttribute->GetValue() ?></p>
+                            <p class="cart-attr mx-1 mb-0"><?= $variantAttribute->GetValue() ?></p>
                             <?php endforeach;?>
-                        <div class="quantity-select"><?= $cartProductDisplay->cartProduct->GetQuantity() ?></div>
+                        <div class="quantity-select mx-1 mb-0"><?= $cartProductDisplay->cartProduct->GetQuantity() ?></div>
                         <div>
                             <button onclick="ReduceQuantity(<?= $cartProductDisplay->cartProduct->GetId() ?>)" class="btn btn-light">-</button>
                             <button onclick="AddQuantity(<?= $cartProductDisplay->cartProduct->GetId() ?>)" class="btn btn-light">+</button>
                             
                         </div>
-                        <button class="delete-cart-item ">Supprimer</button>
+                        <button class="btn btn-danger delete-cart-item ">Supprimer</button>
                     </div> 
                 <?php endforeach;endif; ?>
            </div>
            <div class="cart-confirm d-flex justify-content-center align-items-center mt-5">
                <a class="nav-link" href="./checkout.php">
-                <button>Procéder au payement</button>
+                <button class="btn btn-primary">Procéder au payement</button>
             </a>
            </div>
         
--- front/login.php
+++ front/login.php
@@ -25,10 +25,10 @@
                     <h2>Se connecter</h2>
 
                     <label for="email">Email</label>
-                    <input id="email"type="email" name="email" required>
+                    <input id="email"type="email" name="email" required autocomplete="email">
 
                     <label for="password">Mot de passe</label>
-                    <input id="password" type="password" name="password" required>
+                    <input id="password" type="password" name="password" required autocomplete="current-password">
 
                     <button id="submit-button"type="submit">Se connecter</button>
                     <div id="registerMsg" class="mt-3 text-center"></div>
